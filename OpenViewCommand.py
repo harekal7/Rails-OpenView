dbae59a8787b23380f1b31ec64d816d9d2aefeac
Vikyath Harekal <vikyath.harekal@infibeam.net>
2014-09-25 17:08:50 +0530

    chaged to join

--- OpenViewCommand.py
+++ OpenViewCommand.py
@@ -10,13 +10,7 @@ class OpenViewCommand(sublime_plugin.WindowCommand):
 			base_path = view.file_name().split('/')[:-2]
 			base_path = "/".join(base_path)
 			view_names = view.file_name().split('/')[-1].split('_')[:-1]
-			i = 0
-			view_name = ''
-			for name in view_names:
-				if i != 0:
-					view_name = view_name + "_"	
-				view_name = view_name+name
-				i = i + 1
+			view_name = "_".join(view_names)
 			file_name = base_path+"/views/"+view_name+"/"+view.substr(s)+".html.erb"
 			print (file_name)
 		buffer = window.open_file(file_name)
