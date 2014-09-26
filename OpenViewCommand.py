68a9e9e827de11856b666998e42c2e2b42fc545e
Vikyath Harekal <harekal.vikyath@gmail.com>
2014-09-26 15:39:18 +0530

    changed selected-text to cursor-on-word

--- OpenViewCommand.py
+++ OpenViewCommand.py
@@ -9,5 +9,5 @@ class OpenViewCommand(sublime_plugin.WindowCommand):
 			base = view.file_name().split('controllers/')
 			base_path = "/".join(base[:-1])
 			middle_path = base[-1].split("_controller")[0]
-			file_name = base_path+"views/"+middle_path+"/"+view.substr(text)+".html.erb"
+			file_name = base_path+"views/"+middle_path+"/"+view.substr(view.word(text))+".html.erb"
 		buffer = self.window.open_file(file_name)
