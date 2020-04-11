635f44f786d733004c1da32d6ec7b066d190959d
Marek Příhoda <156730+maprihoda@users.noreply.github.com>
2020-04-11 05:03:50 +0200

    Allow for more extensions to be openable (#1)

--- RailsOpenViewCommand.py
+++ RailsOpenViewCommand.py
@@ -6,18 +6,22 @@ import re
 class RailsOpenViewCommand(sublime_plugin.WindowCommand):
 
 	search_expressions = (r'def +%s',
-					  r'redirect_to +:action +=> +:%s',
+					  					r'redirect_to +:action +=> +:%s',
                       r'redirect_to +:action +=> +"%s"',
                       r"redirect_to +:action +=> +'%s'",
+                      r"redirect_to +action: +'%s'",
                       r'redirect_to +"%s"',
                       r"redirect_to +'%s'",
                       r'redirect_to +:%s',
                       r'render +:action +=> +:%s',
                       r'render +:action +=> +"%s"',
                       r"render +:action +=> +'%s'",
+                      r"render +action: +'%s'",
                       r'render +"%s"',
                       r"render +'%s'",
                       r"render +:%s")
+
+	extensions = ('.html.erb', '.json.jbuilder', '.js.erb', '.slim', '.html.haml', '.js.haml')
 
 	def search(self, view, text):
 		word = view.substr(view.word(text))
@@ -29,12 +33,19 @@ class RailsOpenViewCommand(sublime_plugin.WindowCommand):
 
 	def get_view(self, view, text):
 		base = view.file_name().split('controllers/')
-		if len(base)>1:
+
+		if len(base) > 1:
 			base_path = os.path.join(base[:-1])
-			print (base_path)
 			middle_path = base[-1].split("_controller")[0]
-			file_name = base_path[0]+"views/"+middle_path+"/"+view.substr(view.word(text))+".html.erb"
-			buffer = self.window.open_file(file_name)
+
+			file_name_sans_extension = base_path[0] + "views/" + middle_path + "/" + view.substr(view.word(text))
+
+			for ext in self.extensions:
+				file_name = file_name_sans_extension + ext
+				if os.path.exists(file_name):
+					break
+
+			self.window.open_file(file_name)
 
 	def run(self):
 		view = self.window.active_view()
